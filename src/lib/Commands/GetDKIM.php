d999d36db737db4b14edf66262ed67e90ce4edd9
Clay Freeman <git@clayfreeman.com>
2017-06-21 11:00:39 -0500

    add missing semicolon

--- src/lib/Commands/GetDKIM.php
+++ src/lib/Commands/GetDKIM.php
@@ -51,7 +51,7 @@
       // Query the database for a DKIM record for the requested domain
       $statement = $this->db->prepare('SELECT `dkim_record`, FROM '.
         '`hosting_schema`.`domains` WHERE `name` LIKE :input LIMIT 0,1');
-      $statement->execute([':input' => $input])
+      $statement->execute([':input' => $input]);
       // Fetch the results from the prepared statement
       return $statement->fetch()['dkim_record'];
     }
