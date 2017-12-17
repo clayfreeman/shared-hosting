c774b70a1cbbb2cdb8ebfbed2348a6981763dad2
Clay Freeman <git@clayfreeman.com>
2017-12-17 15:57:31 -0600

    create master branch

--- src/lib/Interfaces/Service.php
+++
@@ -1,8 +0,0 @@
-<?php
-  namespace SharedHosting\Interfaces;
-
-  interface Service {
-    public function fetchResult(): bool;
-    public function     forward(): void;
-    public function     reverse(): void;
-  }
